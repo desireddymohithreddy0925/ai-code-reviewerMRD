26eae530c9ae52ecaac9f538e30c1b902c0d72bb
ionfwsrijan <ionfwsrijan@users.noreply.github.com>
2026-07-23 09:56:20 +0530

    fix: add missing GitHubProvider and GitLabProvider imports in github-action/index.js
    Closes #2814

--- github-action/index.js
+++ github-action/index.js
@@ -5,6 +5,8 @@ import { parseDiff } from './utils/diffParser.js';
 import { scanSecretsInChanges } from './utils/secretsScanner.js';
 import { globToRegex } from './utils/globToRegex.js';
 import { cleanAndParseJSON, normalizeReviewLineNumber } from './utils/actionUtils.js';
+import { GitHubProvider } from './providers/GitHubProvider.js';
+import { GitLabProvider } from './providers/GitLabProvider.js';
 
 const PARSE_FAILED = { reviews: [], _parseFailed: true };
 
